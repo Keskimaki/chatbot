c11dc9b1ec6663051f3942ef99b98f9370d8e6ee
Miko Keskimaki <miko.keskimaki@helsinki.fi>
2023-12-01 17:14:51 +0200

    Logger, db connection

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const inDevelopment = process.env.NODE_ENV === 'development'
+
+export const inStaging = process.env.STAGING === 'true'
+
+export const inProduction = !inStaging && process.env.NODE_ENV === 'production'
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { PORT } from './util/config.js'
+import { connectToDatabase } from './db/connection.js'
 import router from './routes/main.js'
 
 const app = express()
@@ -8,4 +9,8 @@ const app = express()
 app.use('/api', (req, res, next) => router(req, res, next))
 app.use('/api', (_, res) => res.sendStatus(404))
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, async () => {
+  await connectToDatabase()
+
+  console.log(`Server running on port ${PORT}`)
+})
