1ee0ac103af39474ffecfe8015f8465713acc881
Miko Keskimaki <miko.keskimaki@helsinki.fi>
2023-12-02 13:05:14 +0200

    Init i18n

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -10,7 +11,9 @@ import '@fontsource/roboto/700.css'
 
 import App from './App.tsx'
 import queryClient from './util/queryClient'
-import CssBaseline from '@mui/material/CssBaseline'
+import initializeI18n from './util/i18n.ts'
+
+await initializeI18n()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/client/util/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import fi from '../locales/fi.json'
+import en from '../locales/en.json'
+
+declare global {
+  interface Window {
+    __i18n__: typeof i18n
+  }
+}
+
+const initializeI18n = () =>
+  i18n.use(initReactI18next).init({
+    resources: {
+      en,
+      fi,
+    },
+    lng: 'fi',
+    fallbackLng: 'fi',
+    defaultNS: 'common',
+  })
+
+window.__i18n__ = i18n
+
+export default initializeI18n
